3210d545bf8229916e7c83f2d3fd2b1bcde3a016
carol-kangara <carolkangara30@gmail.com>
2021-03-09 07:10:14 +0300

    add post forms

--- app/main/forms.py
+++ app/main/forms.py
@@ -2,3 +2,10 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField,BooleanField
 from wtforms.validators import Required,Length,Email,EqualTo
 
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[Required()])
+    post = TextAreaField('Pitch', validators=[Required()])
+    category = SelectField('Category', choices=[('product', 'product'), ('idea', 'idea'), ('business', 'business')],
+                           validators=[Required()])
+    submit = SubmitField('Post')
+    
