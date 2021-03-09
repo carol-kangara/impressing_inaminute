f646843ae011ae24f48e8dc15acdd8bf6f709a74
carol-kangara <carolkangara30@gmail.com>
2021-03-09 07:32:08 +0300

    add votr form

--- app/main/forms.py
+++ app/main/forms.py
@@ -11,3 +11,5 @@ class PostForm(FlaskForm):
 class CommentForm(FlaskForm):
     comment = TextAreaField('Comment', validators=[Required()])
     submit = SubmitField('Post')
+class Vote(FlaskForm):
+    submit = SelectField('Like')
